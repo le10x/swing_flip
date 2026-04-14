38cdd41c8567ff1f14ec47e74a30916ce7747434
le10x <leanderdrb@gmail.com>
2026-04-14 11:46:54 -0400

    Refactor player frame update and gravity handling

--- src/main.cpp
+++ src/main.cpp
@@ -4,30 +4,27 @@
 using namespace geode::prelude;
 
 class $modify(PlayerObject) {
-    // Usamos updatePlayerFrame para forzar la rotación visual
     void updatePlayerFrame(int frame) {
+        // Llamamos al original
         PlayerObject::updatePlayerFrame(frame);
 
+        // Si es el Swing, tomamos el control visual
         if (this->m_isSwing) {
-            // Buscamos el objeto visual principal (m_iconSprite)
-            if (auto icon = this->m_iconSprite) {
+            // En lugar de setFlipY en el sprite, escalamos el Layer principal
+            // Esto invierte TODO el conjunto (icono, ojos, glow) a la vez
+            if (auto layer = this->m_mainLayer) {
                 if (this->m_isUpsideDown) {
-                    // En gravedad invertida, sumamos 180 grados a la rotación actual
-                    // O simplemente forzamos la inversión visual
-                    icon->setFlipY(true);
+                    layer->setScaleY(-1.0f);
                 } else {
-                    icon->setFlipY(false);
+                    layer->setScaleY(1.0f);
                 }
             }
-        }
-    }
-
-    // También nos aseguramos de que al cambiar de gravedad se actualice
-    void flipGravity(bool upsideDown, bool p1) {
-        PlayerObject::flipGravity(upsideDown, p1);
-        
-        if (this->m_isSwing && this->m_iconSprite) {
-            this->m_iconSprite->setFlipY(upsideDown);
+            
+            // Importante: Si el icono se duplica, es porque m_iconSprite 
+            // está intentando dibujarse por separado. Lo reseteamos aquí:
+            if (this->m_iconSprite) {
+                this->m_iconSprite->setFlipY(false); 
+            }
         }
     }
 };
